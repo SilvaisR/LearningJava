2fe21805289271bd868d4e52870aafa29900a41e
Silvanis <silvanis.silva@outlook.com>
2022-05-17 17:49:54 -0300

    Initial commit

---
+++ src/main/java/medidas.java
@@ -0,0 +1,13 @@
+public class medidas {
+
+    public static void main(String[] args){
+        System.out.println("Olá mundo!");
+
+        int altura = 6, largura = 3, resultado = altura * largura;
+
+        System.out.println("Uma largura de " + largura + "m², e uma altura de " + altura
+                + "m², o resultado será: " + resultado);
+
+
+    }
+}
