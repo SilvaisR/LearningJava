8aebd41a565be6d38afc5ebecac6d9511fea76f3
Silvanis <silvanis.silva@outlook.com>
2022-05-19 17:32:04 -0300

    Fiz o exercício clássico FizzBuzz em java

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/*
+Nesse exercício, eu preciso fazer um programa que conte até 100, e quando o número for divisível por 3 mostrar fizz,
+quando for divisível por 5 Buzz, e quando for divisível por 3 e 5 FizzBuzz, quando não aparecer nenhuma dessas opções
+retornar apenas o número.
+*/
+public class FizzBuzz {
+    public static void main(String[] args){
+        for(int i=0; i<=100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+}
