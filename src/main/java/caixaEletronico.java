34a7a40f5974521907eb21a44e154358929df616
Silvanis <silvanis.silva@outlook.com>
2022-05-20 15:30:36 -0300

    Começando a fazer o exercício do caixa eletrônico.

---
+++ src/main/java/caixaEletronico.java
@@ -0,0 +1,12 @@
+/*
+Neste exerício eu preciso criar um programa que simule um caixa eletrônico, primeiro mostrando quando a pessoa tem de
+saldo, em seguida perguntando se ela quer sacar, ou depositar, em seguida executar a tarefa depedendo da escolha da
+pessoa.
+ */
+public class caixaEletronico {
+    public static void main(String[] args){
+
+        
+
+    }
+}
