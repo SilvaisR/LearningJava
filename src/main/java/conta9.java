1e42d4c0ae3b2ce020ec4487a2ad2fc15f39eb28
Silvanis <silvanis.silva@outlook.com>
2022-05-23 18:47:01 -0300

    Estou travado nesse exercício conta9,porém eu vou conseguir fazer isso e ir em frente.

--- src/main/java/conta9.java
+++ src/main/java/conta9.java
@@ -1,7 +1,24 @@
 /*
-Nesse exercício eu vou escrever o seguinte programa
+Nesse exercício eu vou escrever o seguinte programa, o usuário digita um número que ele quer que seja contado,
+e digita um segundo número que é até onde ele quer a contagem, então o programa vai contar de 0 até o segundo digito do
+usuário e vai dizer quantas vezes o primeiro número apareceu ao decorrer da contagem.
  */
+import java.util.Scanner;
+
 public class conta9 {
+    public static void main(String[] args){
 
+        System.out.print("Digite aqui o primeiro número: ");
+        int opcaoUser1 = scan.nextInt();
+        System.out.print("\nDigite aqui o segundo número: ");
+        int opcaoUser2 = scan.nextInt();
 
+        int contador;
+        for(int i=0; i<=opcaoUser2; i++){
+            String num = Integer.toString(i);
+            char numArr [] = num.toCharArray();
+            for
+
+        }
+    }
 }
