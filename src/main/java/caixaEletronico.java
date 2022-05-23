9693c79dfad8510ed3dbd69eaa193a0d7b982d89
Silvanis <silvanis.silva@outlook.com>
2022-05-23 11:57:35 -0300

    Estou iniciando meu exerício de construi um caixa eletrônico em Java.

--- src/main/java/caixaEletronico.java
+++ src/main/java/caixaEletronico.java
@@ -3,10 +3,32 @@ Neste exerício eu preciso criar um programa que simule um caixa eletrônico, pr
 saldo, em seguida perguntando se ela quer sacar, ou depositar, em seguida executar a tarefa depedendo da escolha da
 pessoa.
  */
-public class caixaEletronico {
-    public static void main(String[] args){
+import java.util.Scanner;
 
-        
+public class caixaEletronico {
+    Scanner scan = new Scanner(System.in);
+    static double saldoEmContaInicial = 600;
+    public static void main(String[] args){
+        double saldo = saldoEmContaInicial;
 
     }
+
+    public static double depositar(double valor){
+        saldoEmContaInicial += valor;
+        return saldoEmContaInicial;
+    }
+
+    public static double sacar(double valor){
+        saldoEmContaInicial -= valor;
+        return valor;
+    }
+
+    // função de empréstimo
+
+    // função de pagar boleto
+
+    // função de solicitar cartão de crédito
+
+
+
 }
