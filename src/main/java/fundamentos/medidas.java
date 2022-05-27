fe3f23a268402065d35218d38c0db51daa5fdd27
Silvanis <silvanis.silva@outlook.com>
2022-05-27 11:56:39 -0300

    Ainda estou nesta aula de medidas, vou só fazer uma pausa para o almoço.

--- src/main/java/fundamentos/medidas.java
+++ src/main/java/fundamentos/medidas.java
@@ -1,4 +1,16 @@
+// 1- Pacote
 package fundamentos;
 
+// 2 - Bibliotecas
+
+
+// 3 - Classes
 public class medidas {
+    // 3.1 - Atributos
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Escolha o cálculo desejado: ");
+
+    }
 }
