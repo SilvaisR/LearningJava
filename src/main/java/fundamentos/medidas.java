b1eac258da3d22055594685a5460d994424e3a8c
Silvanis <silvanis.silva@outlook.com>
2022-05-29 12:28:25 -0300

    Estou fazendo o exercício de medidas e já fiz duas funções, a de calcular o quadrado e a de calcular o retângulo.

--- src/main/java/fundamentos/medidas.java
+++ src/main/java/fundamentos/medidas.java
@@ -2,7 +2,7 @@
 package fundamentos;
 
 // 2 - Bibliotecas
-
+import java.util.Scanner;
 
 // 3 - Classes
 public class medidas {
@@ -10,10 +10,43 @@ public class medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
         System.out.println("Escolha o cálculo desejado: ");
-        System.out.println("(1) - Área do quadrado");
-        System.out.println("(2) - Área do retângulo");
-        System.out.println("(3) - Área do triângulo");
-        System.out.println("(4) - Área do circulo");
+        System.out.println("(1) - Area do quadrado");
+        System.out.println("(2) - Area do retângulo");
+        System.out.println("(3) - Area do triângulo");
+        System.out.println("(4) - Area do circulo");
+        System.out.print("Digite aqui sua opção: ");
+        int opcao = scan.nextInt();
+        System.out.println();
+
+        switch(opcao){
+            case 1:
+                System.out.println("Digite O valor do lado de um dos lados do quadrado para que possamos fazer a conta.");
+                System.out.print("Opção: ");
+                double lado = scan.nextDouble();
+                double resultado1 = calculaAreaQuadrado(lado);
+                System.out.println("\nO resultado do cálculo da área é: " + resultado1);
+                break;
+
+            case 2:
+                System.out.println("Digite os valores para que possamos calcular o retângulo.");
+                System.out.print("Opção 1: ");
+                double lado1 = scan.nextDouble();
+                System.out.print("\nOpção 2: ");
+                double lado2 = scan.nextDouble();
+                double resultado2 = calcularAreaRetangulo(lado1, lado2);
+                System.out.println("\nO resultado do cálculo da área do retângulo é de: " + resultado2);
+
+
+        }
+    }
+
+    public static double calculaAreaQuadrado(double valor){
+        return valor * valor;
+    }
+
+    public static double calcularAreaRetangulo(double valor1, double valor2){
+        return valor1 * valor2;
     }
 }
